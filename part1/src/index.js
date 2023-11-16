dea05ce27a4e21a8b3bb75a329dd214c50f1015f
SalvadorSimonDev <salvador.simon.88@gmail.com>
2023-11-16 11:44:48 +0100

    Añadiendo ejercicio 1.2

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client';
 
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </div>
+  )
+}
+
 const Header = (props) => {
   return (
     <div>
@@ -12,15 +23,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
     </div>
   )
 }
@@ -34,6 +39,9 @@ const Total = (props) => {
 }
 
 
+
+
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -45,17 +53,17 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course}/>
-      <Content 
+      <Header course={course} />
+      <Content
         part1={part1}
         exercises1={exercises1}
-        part2={part2} 
+        part2={part2}
         exercises2={exercises2}
-        part3={part3} 
-        exercises3={exercises3}/>
-      <Total 
-        exercises1={exercises1}  
-        exercises2={exercises2}  
+        part3={part3}
+        exercises3={exercises3} />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
         exercises3={exercises3} />
     </div>
   )
